f7ce3b0167d708d7e129862e4704fc481864db74
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-12-24 00:26:53 +0800

    Update newsk5.sh

--- newsk5.sh
+++ newsk5.sh
@@ -1,4 +1,2 @@
 apt-get update -y && apt-get install curl -y
 wget -q -N --no-check-certificate https://raw.githubusercontent.com/zyhut200/ss5/main/install.sh && chmod 777 install.sh && bash install.sh
-1
-y
