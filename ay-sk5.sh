1ad8837e73eac819920d51d23d44800ecc41d1ac
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-16 15:44:19 +0800

    Update ay-sk5.sh

--- ay-sk5.sh
+++ ay-sk5.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
 ### BEGIN INIT INFO
-# Provides:          Dante SOCKS5 Proxy
+# Provides:          Brook-pf
 # Required-Start:    $network $local_fs $remote_fs
 # Required-Stop:     $network $local_fs $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Dante SOCKS5 Proxy
-# Description:       Start or stop the Dante SOCKS5 Proxy
+# Short-Description: Lightweight port forwarding tool
+# Description:       Start or stop the Brook-pf
 ### END INIT INFO
 
 NAME="ay-sk5"
-DAEMON="/usr/sbin/sockd"
-CONF="/etc/danted.conf"
-PIDFILE="/var/run/$NAME.pid"
-LOGFILE="/var/log/socks.log"
-
-[ -x $DAEMON ] || exit 0
+NAME_BIN="sk5"
+FILE="/usr/local/ay-sk5"
+CONF="${FILE}/ay-sk5.conf"
+LOG="${FILE}/ay-sk5.log"
 
 do_start(){
-    if [ ! -f $CONF ]; then
-        echo "Configuration file does not exist, please check!"
+    if [ ! -f "${CONF}" ]; then
+        echo "配置文件不存在，请检查！"
         exit 1
     fi
-    start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON -- -D
-    echo "$NAME started."
+
+	while read -r line
+    do
+        eval nohup "$FILE/ay-sk5" "$line" >> "${LOG}" 2>&1 &
+    done < "${CONF}"
+    echo "${NAME} 已启动。"
 }
 
 do_stop(){
-    start-stop-daemon --stop --quiet --pidfile $PIDFILE
-    echo "$NAME stopped."
+    pkill -f ay-sk5
+    echo "${NAME} 已停止。"
 }
 
 do_status(){
-    start-stop-daemon --status --quiet --pidfile $PIDFILE
-    case "$?" in
-        0) echo "$NAME is running." ;;
-        1) echo "$NAME is not running and the pid file exists." ;;
-        3) echo "$NAME is not running." ;;
-        4) echo "Unable to determine the status of $NAME." ;;
-    esac
+    if pgrep -f ay-sk5 > /dev/null; then
+        echo "${NAME} 正在运行。"
+    else
+        echo "${NAME} 已停止。"
+    fi
 }
 
 do_restart(){
     do_stop
     sleep 2
     do_start
-    echo "$NAME restarted."
+    echo "${NAME} 已重启。"
 }
 
 case "$1" in
@@ -54,7 +54,8 @@ case "$1" in
         do_$1
         ;;
     *)
-        echo "Usage: $0 { start | stop | restart | status }"
-        exit 1
+        echo -e "使用方法: $0 { start | stop | restart | status }"
+        RETVAL=1
         ;;
 esac
+exit $RETVAL
